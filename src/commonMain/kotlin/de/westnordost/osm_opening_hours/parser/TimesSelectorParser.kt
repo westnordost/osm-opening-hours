701dc004b6149146585d58ca7f2fda708b5edb3f
Tobias Zwick <newton@westnordost.de>
2025-11-19 16:55:08 +0100

    clarify comment

--- src/commonMain/kotlin/de/westnordost/osm_opening_hours/parser/TimesSelectorParser.kt
+++ src/commonMain/kotlin/de/westnordost/osm_opening_hours/parser/TimesSelectorParser.kt
@@ -43,8 +43,9 @@ internal fun StringWithCursor.parseIntervalMinutes(lenient: Boolean): IntervalMi
 internal fun StringWithCursor.parseOffsetTime(lenient: Boolean): ClockTime? {
     val initial = cursor
     val (hourStr, minutesStr) = parseHourMinutes(lenient) ?: return null
-    // for offset time, minutes must be specified because otherwise it would be ambiguous with
-    // IntervalMinutes ("12:00-18:00/5" could otherwise be interpreted as "12:00-18:00/5:00")
+    // for offset time, minutes must be specified also in lenient mode because otherwise it would
+    // be ambiguous with IntervalMinutes ("12:00-18:00/5" could otherwise be interpreted as
+    // "12:00-18:00/5:00")
     if (minutesStr == null) {
         cursor = initial
         return null
